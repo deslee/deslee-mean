454e82badbe85a8ade95ad7c6be9a01a24706dc6
desmond <desmondclee@gmail.com>
2014-03-27 01:56:36 -0500

    fixed server crashing due to empty entry

--- server/configs/models.js
+++ server/configs/models.js
@@ -28,7 +28,7 @@ Model.prototype.trim = function(data) {
 ////
 Model.prototype.out = function(data, req) {
 	var m = this.trim(data);
-	if (req.query.html and m.text) {
+	if (req.query.html && m.text) {
 		m.text = markdown.toHTML(m.text);
 	}
 	if (this.processors.out) {
