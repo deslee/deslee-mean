7e47bd43f46601930b5c5b284fe56cd6f6d56353
desmond <desmondclee@gmail.com>
2014-03-27 00:48:38 -0500

    bg

--- public/js/app.js
+++ public/js/app.js
@@ -74,4 +74,25 @@ cms.run(['$rootScope', '$location', '$window', function($rootScope, $location, $
 	$('.navigation.button').click(function(e) {
 		$('.ui.navigation.sidebar').sidebar('toggle');
 	})
+
+	var random = function(min,max)
+	{
+		return Math.floor(Math.random()*(max-min+1)+min);
+	}
+
+	var animate = function() {
+		var time = random(4000, 6000);
+		var oldx = Number($('body').css('background-position-x').slice(0,-1));
+		var oldy = Number($('body').css('background-position-y').slice(0,-1))
+		console.log('doing animation for ' + time + ' ms from ' + oldx + ', ' + oldy);
+		$('body').animate({
+			'background-position-x': oldx + random(-10,10) + '%',
+			'background-position-y': oldy + random(-10,10) + '%',
+		}, time, 'linear', function() {
+			animate()
+		});
+	}
+
+	animate();
+
 }])
